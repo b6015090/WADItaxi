e3e52a6b1ac4c2e67b8edfbfb27aa3239a0a6334
b6015090 <b6015090@my.shu.ac.uk>
2018-02-16 00:31:58 +0000

    Started current taxi search
    Started adding data to the controller for the current taxi search

--- TaxiAssig/app/Taxi/hompageController.js
+++ TaxiAssig/app/Taxi/hompageController.js
@@ -1,1 +1,20 @@
-﻿angular.module("itsItinerary").controller("itineraryController", function ($scope, $http)
+﻿angular.module("itsItinerary").controller("itineraryController", function ($scope, $http) {
+    $scope.Bookings()[
+        {
+            CurrentPassengers: 1,
+            DropOffLocation:"Sheffield Train Station",
+            Id: 101,
+            PassengerName:"Jane",
+            PassengerLocation:"Peniston",
+            VehicleId:901
+        },
+        {
+            CurrentPassengers: 1,
+            DropOffLocation: "Sheffield Arena",
+            Id: 102,
+            PassengerName: "Jacky",
+            PassengerLocation: "Chesterfield",
+            VehicleId: 902
+        }
+    ];
+});
